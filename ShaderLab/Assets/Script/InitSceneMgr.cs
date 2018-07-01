9586ef65dd9f969bc5ae90235adecbf0bf990376
Sam Lo <sam@randomidea.com>
2018-07-02 00:28:49 +0800

    add init scene to change effect.

---
+++ ShaderLab/Assets/Script/InitSceneMgr.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InitSceneMgr : MonoBehaviour 
+{
+	[SerializeField]
+	private Text m_effectName;
+	private int m_currentIdx = 0;
+
+	private List<string> m_scenes = new List<string>()
+	{
+		{"AdvancedLightingScene"},
+		{"AdvancedTextureMap"},
+		{"BasicScene"},
+		{"BasicTexture"},
+		{"BumpMappingWithSnow"},
+		{"Glass"},
+		{"GrayPostEffect"},
+		{"Hologram"},
+		{"MotionBlur"},
+		{"outline"},
+		{"PhysicBasedRendering"},
+		{"Plasma"},
+		{"StencilBuffer"},
+		{"VertexExtrude"},
+		{"Water"},
+		{"Wave"},
+	};
+
+	void Start () 
+	{
+		loadScene ();
+	}
+
+	void loadScene()
+	{
+		SceneManager.LoadScene (m_scenes[m_currentIdx]);
+		m_effectName.text = string.Format("{0}. {1}",m_currentIdx +1 , m_scenes [m_currentIdx] );
+	}
+
+	public void OnClickNext()
+	{
+		m_currentIdx++;
+		m_currentIdx = m_currentIdx % m_scenes.Count;
+		loadScene ();
+	}
+
+	public void OnClickLast()
+	{
+		m_currentIdx--;
+		if (m_currentIdx < 0)
+		{
+			m_currentIdx = m_scenes.Count - 1;
+		}
+		loadScene ();
+	}
+}
